32cecd00222c6c0f27055a8d0a359044cccbfdab
Nayan17107 <prajapatinayan17107@gmail.com>
2025-10-02 10:07:48 +0530

    02/10/2025

---
+++ React-JS/Practice/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import '../src/App.css'
+import StaticList from './Component/Static'
+import Daynamic from './Component/Dynamic'
+import UseRef from './Component/Fragment-Usr'
+
+function App() {
+  // const [list] = useState(["Home", "Servise", "Blog", "About", "Contact", "Help"])
+  return (
+    <div>
+      {/* <StaticList list={list} /> */}
+      {/* <Daynamic list={list} /> */}
+      {/* <UseRef/> */}
+    </div>
+  )
+}
+
+export default App
