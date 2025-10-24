9eecbd3117f0c89664c942574ac003c9c4147f86
Nayan17107 <prajapatinayan17107@gmail.com>
2025-10-24 11:27:36 +0530

    24/102025

--- React-JS/Web-Site/src/App.jsx
+++ React-JS/Web-Site/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from './Components/Header/Header.jsx'
+import Contact from './Components/Contact/Contact.jsx';
 import Footer from './Components/Footer/Footer.jsx'
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
 
 function App() {
   return (
     <>
       <Header/>
+      <Contact/>
       <Footer/>
     </>
   )
