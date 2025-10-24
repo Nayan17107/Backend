73fa9e842f16eecd912d00f2c0853b076df4d315
Nayan17107 <prajapatinayan17107@gmail.com>
2025-10-24 11:11:57 +0530

    24/102025

--- React-JS/Web-Site/src/App.jsx
+++ React-JS/Web-Site/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Header from './Components/Header/Header.jsx'
+import Footer from './Components/Footer/Footer.jsx'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 function App() {
   return (
     <>
-      
+      <Header/>
+      <Footer/>
     </>
   )
 }
 
-export default App
+export default App;
