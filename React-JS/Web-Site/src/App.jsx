16103c7987595472cf037e204aeb44f29134378b
Nayan17107 <prajapatinayan17107@gmail.com>
2025-10-28 11:01:52 +0530

    28/10/2025

--- React-JS/Web-Site/src/App.jsx
+++ React-JS/Web-Site/src/App.jsx
@@ -4,12 +4,14 @@ import Contact from './Components/Contact/Contact.jsx';
 import Footer from './Components/Footer/Footer.jsx'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Home from './Components/Home/Home.jsx';
+import Garden from './Components/Garden-comp/Garden.jsx';
 
 function App() {
   return (
     <>
       <Header/>
-      <Home/>
+      {/* <Home/> */}
+      {/* <Garden/> */}
       <Contact/>
       <Footer/>
     </>
