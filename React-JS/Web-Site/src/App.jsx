3dedf4d538d3a47221d7d6d0b188f125d69d6347
Nayan17107 <prajapatinayan17107@gmail.com>
2025-10-30 10:14:17 +0530

    30/10/2025

--- React-JS/Web-Site/src/App.jsx
+++ React-JS/Web-Site/src/App.jsx
@@ -5,13 +5,17 @@ import Footer from './Components/Footer/Footer.jsx'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Home from './Components/Home/Home.jsx';
 import Garden from './Components/Garden-comp/Garden.jsx';
+import FoodWasteComp from './Components/Food-Waste/Food.jsx';
+import WaterPage from './Components/Water/Water.jsx';
 
 function App() {
   return (
     <>
       <Header/>
-      {/* <Home/> */}
+      <Home/>
       {/* <Garden/> */}
+      {/* <FoodWasteComp/> */}
+      {/* <WaterPage/> */}
       <Contact/>
       <Footer/>
     </>
