2d783e94fa9578226d0a98c634580e22adaef21b
Nayan17107 <prajapatinayan17107@gmail.com>
2025-12-22 11:05:24 +0530

    22/12/25

--- Node-JS/Third-Party-Moduals/index.js
+++ Node-JS/Third-Party-Moduals/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const port = 8080;
 const app = express()
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.end("Welcome to Express.Js")
+})
+
+app.get('/home', (req, res) => {
+    res.end("Welcome to Home-Page")
 })
 
 app.listen(port, () => {
