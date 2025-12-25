0f6538e523b2e58a1a5eb7168dbfb0353d794420
Nayan17107 <prajapatinayan17107@gmail.com>
2025-12-25 10:47:18 +0530

    25/12/25

--- Node-JS/Third-Party-Moduals/index.js
+++ Node-JS/Third-Party-Moduals/index.js
@@ -3,15 +3,45 @@ const port = 8080;
 const app = express()
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded());
+
+const students = [
+    {
+        id : "1",
+        name : "Prajapati Nayan",
+        age : "19",
+        email : "nayan123@.in"
+    },
+    {
+        id : "2",
+        name : "Pajiyavala Krish",
+        age : "19",
+        email : "krish123@.in"
+    },
+    {
+        id : "3",
+        name : "Dhamiliya Luv",
+        age : "21",
+        email : "luv123@.in"
+    },
+    {
+        id : "4",
+        name : "Trapsiya Meet",
+        age : "19",
+        email : "meet123@.in"
+    },
+]
 
 app.get('/', (req, res) => {
-    res.end("Welcome to Express.Js")
+    res.render('index', {students})
 })
 
-app.get('/home', (req, res) => {
-    res.render('index')
+app.post('/add-student', (req, res) =>{
+    students.push(req.body);
+    console.log(req.body)
+    return res.redirect('/')
 })
 
 app.listen(port, () => {
-    console.log(`Server is started at ${port}`);
+    console.log(`Server is started at http://localhost:${port}`);
 });
