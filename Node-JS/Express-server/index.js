1a66d784abe122dab715f82dbff99e345a3dc6fa
Nayan17107 <prajapatinayan17107@gmail.com>
2025-12-29 11:02:16 +0530

    29/12/25

--- Node-JS/Express-server/index.js
+++ Node-JS/Express-server/index.js
@@ -3,40 +3,41 @@ const port = 8080;
 const app = express()
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded());
+app.use(express.urlencoded())
+app.use(express.json())
 
 let students = [
     {
-        id : "1",
-        name : "Prajapati Nayan",
-        age : "19",
-        email : "nayan123@.in"
+        id: "1",
+        name: "Prajapati Nayan",
+        age: "19",
+        email: "nayan123@.in"
     },
     {
-        id : "2",
-        name : "Pajiyavala Krish",
-        age : "19",
-        email : "krish123@.in"
+        id: "2",
+        name: "Pajiyavala Krish",
+        age: "19",
+        email: "krish123@.in"
     },
     {
-        id : "3",
-        name : "Dhamiliya Luv",
-        age : "21",
-        email : "luv123@.in"
+        id: "3",
+        name: "Dhamiliya Luv",
+        age: "21",
+        email: "luv123@.in"
     },
     {
-        id : "4",
-        name : "Trapsiya Meet",
-        age : "19",
-        email : "meet123@.in"
+        id: "4",
+        name: "Trapsiya Meet",
+        age: "19",
+        email: "meet123@.in"
     },
 ]
 
 app.get('/', (req, res) => {
-    res.render('index', {students})
+    res.render('index', { students })
 })
 
-app.post('/add-student', (req, res) =>{
+app.post('/add-student', (req, res) => {
     // console.log(req.body)
     students.push(req.body);
     return res.redirect('/')
@@ -48,6 +49,32 @@ app.get('/delete-student/:id', (req, res) => {
 
     students = students.filter(stud => stud.id !== id)
     return res.redirect('/')
+})
+
+app.get('/edit-student/:id', (req, res) => {
+    // console.log(req.params.id);
+
+    let id = req.params.id;
+    student = students.find((stud) => stud.id == id);
+    return res.render('edit-student', { student })
+})
+
+app.post('/update-student/:id', (req, res) => {
+    let id = req.params.id;
+
+    let update = students.map((stud) => {
+        if (stud.id == id) {
+            return {
+                ...req.body,
+                id: id,
+            }
+        } else {
+            return stud
+        }
+    })
+
+    students = update
+    res.redirect('/')
 })
 
 app.listen(port, () => {
